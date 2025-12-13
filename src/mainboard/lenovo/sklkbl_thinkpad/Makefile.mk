e54ce219da76b66a00492c398db0cd27b5624f11
MIla <mila.gastelum@autistici.org>
2025-12-13 08:23:37 +0000

    Use t480s spd binaries

--- src/mainboard/lenovo/sklkbl_thinkpad/Makefile.mk
+++ src/mainboard/lenovo/sklkbl_thinkpad/Makefile.mk
@@ -21,11 +21,11 @@ $(foreach n,$(SPD_BINS),$(eval $(call SPD_template,$(n))))
 endif
 
 # Add files spd_0.bin to spd_9.bin to the cbfs image
-ifeq ($(CONFIG_BOARD_LENOVO_T480S),y)
+ifeq ($(CONFIG_BOARD_LENOVO_T460S),y)
 SPD_BINS := $(shell seq 0 9)
 define SPD_template
 cbfs-files-y += spd_$(1).bin
-spd_$(1).bin-file := variants/$(VARIANT_DIR)/spd/spd_$(1).bin
+spd_$(1).bin-file := variants/t480s/spd/spd_$(1).bin
 spd_$(1).bin-type := raw
 endef
 $(foreach n,$(SPD_BINS),$(eval $(call SPD_template,$(n))))
